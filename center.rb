cbbe528b25ea46b8c0c46b866ff6776c5135c7dc
Redwingshunt <alohaass12@gmail.com>
2024-02-21 14:52:06 +0530

    intial commit

---
+++ center.rb
@@ -0,0 +1,18 @@
+s = "this_String_willbe_centered"
+total_length = 50
+
+result = s.center(total_length, "*")   # this will be centered by '*'
+puts result
+
+s = "this_String_willbe_centered"
+total_length = 50
+
+result = s.center(total_length, "$")  # this will be centered by '$'
+puts result
+
+
+s = "this_String_willbe_centered"
+total_length = 50
+
+result = s.center(total_length, "#") # this will be centered by '#'
+puts result
